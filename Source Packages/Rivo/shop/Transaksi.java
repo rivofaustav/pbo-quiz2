c189e812e13a2663bcfe61228c6dd2bad35d75b2
Rivo <rivofaustav.rv@gmail.com>
2018-11-28 22:08:42 +0700

    mengerjakan class transaksi menghitung total dan print nota

--- Source Packages/Rivo/shop/Transaksi.java
+++ Source Packages/Rivo/shop/Transaksi.java
@@ -1,13 +1,42 @@
 
 package Rivo.shop;
 
+import java.util.*;
+
 /**
  *
  * @author Rivo
  */
-import java.util.*;
 
 public class Transaksi {
+    private final String code; //variabel code
+    private ArrayList<Item> items = new ArrayList<>();//variabel item
+    private float total;//variabel total
+
+    public Transaksi(String code, ArrayList<Item> items) {
+        this.code = code;
+        this.items = items;
+    }
     
+    public void setTotal(){//setter total
+        float total = 0;
+        for (Item item : this.items){
+            total += item.getTotal();
+        }
+        this.total = total;
+    }
+    
+    
+    public String print(){//print nota output
+        setTotal();
+        String str = "";
+        str += "Kode \t\t: " +this.code+"\n";
+        str += "Daftar Belanja : \n";
+        for(Item item : this.items){
+            str += "\t" + item.getNama() + "(x" + item.getJumlah()+") : " + item.getTotal() + "\n";
+        }
+        str += "TOTAL HARGA \t: " + this.total;
+        return str;
+    }
     
 }
